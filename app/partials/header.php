9a7fff0ab1bcb22515bb91d333784d54f26e59b1
dallari nicolas <nicogamer@hotmail.fr>
2025-02-05 15:12:14 +0100

    added security file, more meta for seo

--- app/partials/header.php
+++ app/partials/header.php
@@ -3,14 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Calculez facilement l'âge de votre animal préféré avec notre outil en ligne.">
-    <meta name="keywords" content="âge animal, calcul âge, chat, chien, animal de compagnie">
-    <meta name="author" content="Dallari nicolas">
+    <meta name="description" content="Découvrez l'âge réel de votre animal de compagnie grâce à notre outil de calcul en ligne précis et facile à utiliser.">
+    <meta name="keywords" content="âge animal, calcul âge animal, âge chien, âge chat, conversion âge animal, animal de compagnie">
+    <meta name="author" content="Dallari Nicolas">
     <meta name="robots" content="index, follow">
-    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
+    <meta property="og:title" content="Calculateur d'âge pour animaux - Âge Animaux">
+    <meta property="og:description" content="Transformez l'âge de votre chien ou chat en années humaines avec notre outil simple et rapide.">
+    <meta property="og:url" content="https://www.age-animaux.dallarinicolas.fr/">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://www.age-animaux.dallarinicolas.fr/assets/images/preview.jpg">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Calculateur d'âge pour animaux - Âge Animaux">
+    <meta name="twitter:description" content="Calculez facilement l'âge de votre animal préféré en années humaines.">
+    <meta name="twitter:image" content="https://www.age-animaux.dallarinicolas.fr/assets/images/preview.jpg">
+    <link rel="canonical" href="https://www.age-animaux.dallarinicolas.fr/">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lavishly+Yours&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
 </head>
 <body>
 <nav class="mobile">
